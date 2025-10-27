0a9cd85543cd9157316d030e8557e0dbe9c00886
Illia Malachyn <illia.malachyn@gmail.com>
2025-10-27 12:49:33 +0200

    Add specs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { AppService } from './app.service';
 import { TemperatureSensorsModule } from './temperature-sensors/temperature-sensors.module';
 import { HumiditySensorsModule } from './humidity-sensors/humidity-sensors.module';
 import { LightSensorsModule } from './light-sensors/light-sensors.module';
+import { TemperatureSensorsController } from './temperature-sensors/temperature-sensors.controller';
+import { LightSensorsController } from './light-sensors/light-sensors.controller';
+import { TemperatureSensorsService } from './temperature-sensors/temperature-sensors.service';
+import { LightSensorsService } from './light-sensors/light-sensors.service';
 
 @Module({
   imports: [TemperatureSensorsModule, HumiditySensorsModule, LightSensorsModule],
