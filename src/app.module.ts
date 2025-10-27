3b7eff1953b0922f6ddc8873c0f059745b863620
Illia Malachyn <illia.malachyn@gmail.com>
2025-10-27 14:45:35 +0200

    add logging middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { LoggingMiddleware } from './logging.middleware';
 import { TemperatureSensorsModule } from './temperature-sensors/temperature-sensors.module';
 import { HumiditySensorsModule } from './humidity-sensors/humidity-sensors.module';
 import { LightSensorsModule } from './light-sensors/light-sensors.module';
-import { TemperatureSensorsController } from './temperature-sensors/temperature-sensors.controller';
-import { LightSensorsController } from './light-sensors/light-sensors.controller';
-import { TemperatureSensorsService } from './temperature-sensors/temperature-sensors.service';
-import { LightSensorsService } from './light-sensors/light-sensors.service';
 
 @Module({
   imports: [TemperatureSensorsModule, HumiditySensorsModule, LightSensorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
